798308218b931e1bea47aba72b337cbb44342b40
nicks <simtaokai@hotmail.com>
2022-08-14 21:01:00 +0100

    first commit

---
+++ WebAPI/Models/BugDBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class BugDBContext:DbContext
+    {
+        public BugDBContext(DbContextOptions<BugDBContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
+
---
+++ WebAPI/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Column(TypeName = "nvarchar(100")]
+        public string fullName { get; set; }
+
+        [Column(TypeName = "nvarchar(100")]
+        public string email { get; set; }
+    }
+
+
+}
